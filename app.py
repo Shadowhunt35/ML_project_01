d1ded0768f48c062b33209199f97d8bb4112ada4
Shadowhunt35 <skumarsinha505@gmail.com>
2026-01-03 01:34:12 +0530

    Prediction Pipeline

--- app.py
+++ app.py
@@ -23,14 +23,14 @@ def predict_datapoint():
         
         data = CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
+            race_ethnicity=request.form.get('race_ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('reading_score')),
             writing_score=float(request.form.get('writing_score'))
         )
-        
+
         pred_df = data.get_data_as_data_frame()
         print(pred_df)
 
@@ -39,6 +39,6 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
     
     
